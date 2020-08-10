dc4565f2b05be87746b9b68f97490915de572763
ErisedKT <sanyaverma0303@gmail.com>
2020-08-10 12:13:15 +0530

    Add DM function

--- snipe_bot.py
+++ snipe_bot.py
@@ -1,4 +1,4 @@
-# Usage -<track/untrack> <CRN>
+# Usage $<track/untrack> <CRN>
 
 import discord
 from oscar_scraper import *
@@ -17,6 +17,8 @@ def add_class(user, crn):
 
 def remove_class(user, crn):
     tracking[crn].remove(user)
+    if len(tracking[crn]) == 0:
+        del tracking[crn]
 
 tracking = {}
 class_status = {}
@@ -31,7 +33,7 @@ async def on_ready():
 async def track(ctx, crn):
     try:
         status = class_status(scrape(crn))
-        add_class(ctx.author.id, crn)
+        add_class(ctx.author, crn)
         class_status[crn] = status
         await ctx.send(f'{ctx.author.mention} Now tracking {crn}.')
     except:
@@ -40,23 +42,27 @@ async def track(ctx, crn):
 
 @client.command()
 async def untrack(ctx, crn):
-    if crn in tracking and ctx.author.id in tracking[crn]:
-        remove_class(ctx.author.id, crn)
+    if crn in tracking and ctx.author in tracking[crn]:
+        remove_class(ctx.author, crn)
         await ctx.send(f'{ctx.author.mention} Stopped tracking {crn}.')
     else:
         await ctx.send(f'{ctx.author.mention} You are not tracking {crn}. Did you mean "track"?')
 
 def check_for_status_changes():
-    changed_status = []
+    changed_status = {}
     for crn in tracking:
         status = class_status(scrape(crn))
         if status != class_status[crn]:
-            changed_status.append(crn)
+            changed_status[crn] = class_status[crn]
             class_status[crn] = status
     return changed_status
 
-def notify():
-    
+def notify(changed_status):
+    for crn in changed_status:
+        for user in tracking[crn]:
+            user.send("Class %s changed from %s to %s.", crn,
+                      changed_status[crn], class_status[crn])
+
 
 client.run('your-token-here')
 
