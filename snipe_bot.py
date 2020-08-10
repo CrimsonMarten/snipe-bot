eb6c88131f495129e06cfe4825c3addb3bce4ac6
ErisedKT <sanyaverma0303@gmail.com>
2020-08-10 07:32:55 +0530

    Add status change tracking function

--- snipe_bot.py
+++ snipe_bot.py
@@ -6,16 +6,20 @@ from discord.ext import commands
 import requests
 
 def add_class(user, crn):
-    if user in trackers.keys():
-        if crn not in trackers[user]:
-            trackers[user].append(crn)
+    """
+    Precondition: crn is validated
+    """ 
+    if crn in tracking and user not in tracking[crn]:
+        tracking[crn].append(user)
     else:
-        trackers[user] = [crn]
+        tracking[crn] = [user]
+    
 
 def remove_class(user, crn):
-    trackers[user].remove(crn)
+    tracking[crn].remove(user)
 
-trackers = {}
+tracking = {}
+class_status = {}
 
 client = commands.Bot(command_prefix='$')
 
@@ -28,6 +32,7 @@ async def track(ctx, crn):
     try:
         status = class_status(scrape(crn))
         add_class(ctx.author.id, crn)
+        class_status[crn] = status
         await ctx.send(f'{ctx.author.mention} Now tracking {crn}.')
     except:
         await ctx.send(f'{ctx.author.mention} Could not find class with CRN: {crn}.')
@@ -35,19 +40,24 @@ async def track(ctx, crn):
 
 @client.command()
 async def untrack(ctx, crn):
-    user_tracks = trackers.get(ctx.author.id)
-    if user_tracks and crn in user_tracks:
+    if crn in tracking and ctx.author.id in tracking[crn]:
         remove_class(ctx.author.id, crn)
         await ctx.send(f'{ctx.author.mention} Stopped tracking {crn}.')
     else:
         await ctx.send(f'{ctx.author.mention} You are not tracking {crn}. Did you mean "track"?')
 
+def check_for_status_changes():
+    changed_status = []
+    for crn in tracking:
+        status = class_status(scrape(crn))
+        if status != class_status[crn]:
+            changed_status.append(crn)
+            class_status[crn] = status
+    return changed_status
 
-client.run('NzQwNTAxNDA2NzE2Mzk1NjAw.Xyp7rQ.VhoKExn-j8EdvpB3OsnQZgWY0VQ')
+def notify():
+    
 
-class ClassStatus():
-    def __init__(self, crn, status):
-        self.crn = crn
-        self.status = status
+client.run('your-token-here')
 
     
